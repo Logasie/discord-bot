a5c097834e4090d55630fce0a3f4750fcbac921e
Logasie <108527828+Logasie@users.noreply.github.com>
2022-07-02 19:34:04 +0200

    Update interactionCreate.js

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -6,13 +6,6 @@ module.exports = {
     async execute(client, interaction) {
         if (interaction.isCommand() || interaction.isContextMenu()) {
 
-            let guildSettings = await client.getGuild(interaction.guild);
-
-            if(!guildSettings) {
-                await client.createGuild(interaction.guild);
-                guildSettings = await client.getGuild(interaction.guild)
-            }
-
             const cmd = client.commands.get(interaction.commandName);
             if (!cmd) return interaction.reply('Cette commande n\'existe pas');
     
@@ -22,15 +15,15 @@ module.exports = {
     
             if (!interaction.member.permissions.has([cmd.permissions])) return interaction.reply(`Vous n'avez pas la/les permission(s) requise(s) (\`${cmd.permissions.join(', ')}\`) pour taper cette commande`)
     
-            cmd.runInteraction(client, interaction, guildSettings);
+            cmd.runInteraction(client, interaction);
         } else if (interaction.isButton()) {
             const btn = client.buttons.get(interaction.customId);
             if (!btn) return interaction.reply('Ce bouton n\'existe pas');
-            btn.runInteraction(client, interaction, guildSettings);
+            btn.runInteraction(client, interaction);
         } else if (interaction.isSelectMenu()) {
             const selectMenu = client.selects.get(interaction.customId);
             if (!selectMenu) return interaction.reply('Ce menu n\'existe pas');
-            selectMenu.runInteraction(client, interaction, guildSettings);
+            selectMenu.runInteraction(client, interaction);
         }
     },
 };
